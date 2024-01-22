55638627f7a014f37075869ecd34e3f13848fa16
WarisaTT <111375481+WarisaTT@users.noreply.github.com>
2024-01-22 17:50:01 +0700

    Delete Homework directory

--- Homework/Q12.js
+++
@@ -1,9 +0,0 @@
-// 12.// Functions / Arrays / Arrays Functions Instruction: 
-
-// - Write a function uniqueElements(arrayValue) to return array of the unique elements from an array.
-
-// Initiate code:
-
-// let numbers = [1, 1, 3, 8, 21, 8, 13, 3, 8, 7];
-
-// // Insert your code here // array numbers after use function uniqueElements will be [1, 3, 8, 21, 13, 7]
