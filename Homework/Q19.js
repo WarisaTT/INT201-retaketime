3c740404ae31048ecf424134bdb3b74b330b7505
WarisaTT <111375481+WarisaTT@users.noreply.github.com>
2024-01-22 17:48:49 +0700

    Add files via upload

---
+++ Homework/Q19.js
@@ -0,0 +1,19 @@
+// 19. // Expressions, Operators, and Control Structures / Arrays / Arrays Functions / Functions Instruction: 
+
+// - Write a JavaScript function, sumPositiveNumbers(numbers), that calculates the sum of all positive numbers in an array. The function should return the sum of positive numbers. If the array is empty or contains no positive numbers, the function should
+
+// return 0.
+
+// Initiate code:
+
+// // Insert your code here console.log(sumPositiveNumbers([1, -4, 12, 0, -3, 29, -150])); console.log(sumPositiveNumbers([-1, -2, -3])); console.log(sumPositiveNumbers([])); 
+
+// output 
+
+// 42 0
+
+
+
+// 0
+
+
---
+++ Homework/Q5.js
@@ -0,0 +1,16 @@
+// 5. // Types, Values, and Variables/ Arrays Instruction: 
+
+// - Write code to display only the object elements in the mixedArray.
+
+// Initiate code:
+
+// let mixedArray = [42, "hello", {name: “Joe”, surname: “Doe”}, true,56,false, {name: “John”, surname: “Smith”}, {name: “Joy”, surname: “Stein”}];
+
+// // Insert your code here Output: 
+
+// {name: "Joe ", surname: “Doe}
+
+// {name: "John ", surname: “Smith”}
+
+// {name: "Joy ", surname: “Stein”}
+
