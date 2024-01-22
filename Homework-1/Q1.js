ccb374cadc6f56ad4b94fe38cc46b6676b20776d
WarisaTT <111375481+WarisaTT@users.noreply.github.com>
2024-01-22 17:56:22 +0700

    Add files via upload

---
+++ Homework-1/Q1.js
@@ -0,0 +1,14 @@
+// 1. Types, Values, and Variables/ Functions Instruction: 
+
+// - Write function findMin(arr) : Find min number in an array : Return min number Initiate code: 
+
+// let numbers = [34, 1, 58, 8, 21, 5, 13, 3, 89, 7]; console.log(findMin(arr));// 1 
+
+// // Insert your code here
+
+function findMin(arr){
+      
+}
+
+let numbers = [34, 1, 58, 8, 21, 5, 13, 3, 89, 7]; 
+console.log(findMin(arr));
---
+++ Homework-1/Q7.js
@@ -0,0 +1,13 @@
+// 7. // Objects Instruction: 
+
+// - Create an object person with properties: name (a string), age (a number), and isStudent (a boolean).
+
+// 7.1 Add a new property hobbies, an array of strings, to the person object.
+
+// 7.2 Change the age property to a new value.
+
+// 7.3 Delete the isStudent property.
+
+// Initiate code:
+
+// // Insert your code here
